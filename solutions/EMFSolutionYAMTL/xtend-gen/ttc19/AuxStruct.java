3bfe49cd55c3a221ed7dd53bf0197b1621470744
Benjamin Rivard <benjamin.rivard@imt-altantique.net>
2019-10-25 15:16:43 +0200

    doesn't work

---
+++ solutions/EMFSolutionYAMTL/xtend-gen/ttc19/AuxStruct.java
@@ -0,0 +1,15 @@
+package ttc19;
+
+import java.util.Map;
+
+@SuppressWarnings("all")
+public class AuxStruct {
+  /* @Accessors
+   */private final /* Map<Integer, Tree> */Object nodeMap /* Skipped initializer because of errors */;
+  
+  /* @Accessors
+   */private final /* Map<Tree, Integer> */Object nodeIndex /* Skipped initializer because of errors */;
+  
+  /* @Accessors
+   */private final /* Map<Triple<String, Integer, Integer>, Tree> */Object treeMap /* Skipped initializer because of errors */;
+}
